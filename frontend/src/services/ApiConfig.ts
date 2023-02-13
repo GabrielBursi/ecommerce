8db3acba76103616f1e9304269a99e82e3b3c9dd
Gabriel Bursi <gabriellbursii@gmail.com>
2023-02-13 17:01:57 -0300

    mudando a api

--- frontend/src/services/ApiConfig.ts
+++ frontend/src/services/ApiConfig.ts
@@ -5,11 +5,11 @@ export function Api(item: string): Promise<AxiosResponse<any, any>>{
 
     const options = {
         method: 'GET',
-        url: 'https://pricer.p.rapidapi.com/str',
-        params: { q: `${item}` },
+        url: 'https://real-time-amazon-data.p.rapidapi.com/search',
+        params: {query: item, country: 'BR', category_id: 'aps', page: '1'},
         headers: {
             'X-RapidAPI-Key': '625991cf24msh24bb2daeff0312bp11bb56jsn0635b29f1ffd',
-            'X-RapidAPI-Host': 'pricer.p.rapidapi.com'
+            'X-RapidAPI-Host': 'real-time-amazon-data.p.rapidapi.com'
         }
     };
     
