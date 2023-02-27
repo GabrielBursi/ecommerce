6292afb112e19f50b82438f423fd3dc974058959
Gabriel Bursi <gabriellbursii@gmail.com>
2023-02-27 14:28:28 -0300

    colocando variaveis de ambiente na config api

--- frontend/src/services/ApiConfig.ts
+++ frontend/src/services/ApiConfig.ts
@@ -5,13 +5,13 @@ export function Api(item: string): Promise<AxiosResponse<any, any>>{
 
     const options = {
         method: 'GET',
-        url: 'https://real-time-amazon-data.p.rapidapi.com/search',
-        params: {query: item, country: 'BR', category_id: 'aps', page: '1'},
+        url: process.env.API_URL,
+        params: {query: item, country: 'BR'},
         headers: {
-            'X-RapidAPI-Key': '625991cf24msh24bb2daeff0312bp11bb56jsn0635b29f1ffd',
-            'X-RapidAPI-Host': 'real-time-amazon-data.p.rapidapi.com'
+            'X-RapidAPI-Key': process.env.API_KEY,
+            'X-RapidAPI-Host': process.env.API_HOST
         }
     };
     
     return axios.request(options)
-}
+}   
