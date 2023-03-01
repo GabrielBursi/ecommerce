d4fdf480c6a953b90ab15ce56f8ba7144ec93c4d
Gabriel Bursi <gabriellbursii@gmail.com>
2023-03-01 10:34:53 -0300

    fazendo a página de produto

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,34 +1,74 @@
 import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Box, Button, Divider, IconButton, Paper, Rating, TextField, Typography } from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
 import SellIcon from '@mui/icons-material/Sell';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
-import { useParams } from "react-router-dom";
 import { LayoutBase } from "../layouts";
 import { IProducts } from "../types";
-import { ProductsContext } from "../contexts";
+import { LoginContext, ProductsContext } from "../contexts";
 import { Carousel, MyImage } from "../components";
 
 export function ProductPage() {
 
-    const { id } =useParams<'id'>()
+    const { id } = useParams<'id'>()
+    const navigate= useNavigate()
 
-    const [productInfo, setProductInfo] = useState<IProducts>();
+    const { products, setProductsInCart, productsInCart, setProductsLiked, productsLiked } = useContext(ProductsContext)
+    const { isLogged } = useContext(LoginContext)
 
-    const { products } = useContext(ProductsContext)
+    const [productInfo, setProductInfo] = useState<IProducts>({ id: '', img: '', price: '', title: ' ', rating: 0 });
+    const [isFavorite, setIsFavorite] = useState<boolean>(false);
+    const [isAlreadyInCart, setIsAlreadyInCart] = useState<boolean>(false);
 
     useEffect(() => {
-        if(id){
-            const productSelected = products.find(product => product.id === id)
-            setProductInfo(productSelected)
+        if(!id){
+            return console.log('teste');
         }
+        const arrProductSelected = products.filter(product => product.id === id)
+        const [productSelected] = arrProductSelected
+        setProductInfo(productSelected)
+
+        const productLiked = productsLiked.filter(product => product.id === id)
+        productLiked.forEach(() => {
+            setIsFavorite(true)
+        })
+        const productLikedInCart = productsInCart.filter(product => product.id === id)
+        productLikedInCart.forEach(() => {
+            setIsAlreadyInCart(true)
+        })
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [id]);
+    }, [id, productsLiked, productsInCart]);
+
+    function addProductInCart(id: number | string | undefined) {
+
+        if (!isLogged)
+            return navigate('/login')
+
+        setProductsInCart([...productsInCart, { img: productInfo.img, price: productInfo.price, title: productInfo.title, rating: productInfo.rating, id }])
+        navigate(`/precart/${id}`)
+    }
+
+    function addProductInLiked() {
+
+        if (!isLogged)
+            return navigate('/login')
+
+        setIsFavorite(oldIsFavorite => !oldIsFavorite)
+        if (!isFavorite) {
+            setProductsLiked([...productsLiked, { img: productInfo.img, price: productInfo.price, title: productInfo.title, rating: productInfo.rating, id }])
+        } else {
+            const productsLikedWithout = productsLiked.filter(product => product.id !== id)
+            setProductsLiked(productsLikedWithout)
+        }
+    }
 
     return (
         <LayoutBase showActions showResearchInput showTabBar showUserInfo>
             <Box display='flex' justifyContent='center' alignItems='center' height='100%'>
-                <Box component={Paper} elevation={2} width='1570px' height='750px' display='flex' flexDirection='column' padding={2}>
+                <Box component={Paper} elevation={2} width='1270px' height='750px' display='flex' flexDirection='column' padding={2}>
                     <Box width='100%'height='20%'>
                         <Typography 
                             variant="h5" 
@@ -38,7 +78,7 @@ export function ProductPage() {
                             textOverflow='ellipsis'
                             sx={{ wordBreak: 'break-word', display: "-webkit-box", WebkitLineClamp: 3, WebkitBoxOrient: "vertical" }}
                         >
-                            {productInfo?.title}
+                            {productInfo.title}
                         </Typography>
                     </Box>
                     <Box flex={1} width='100%' display='flex'> 
@@ -47,33 +87,33 @@ export function ProductPage() {
                                 <Box width='100%' height='10%' display='flex' justifyContent='space-between'>
                                     <Box width='30%'display='flex' justifyContent='center' alignItems='center'>
                                         <Typography variant="subtitle1" color='black' fontWeight='bold'>
-                                            {productInfo?.title.split(' ')[0]}
+                                            {productInfo.title.split(' ')[0]}
                                         </Typography>
                                     </Box>
                                     <Divider orientation="vertical" flexItem/>
                                     <Box width='30%' display='flex' justifyContent='center' alignItems='center'>
-                                        <Rating value={productInfo?.rating} precision={0.5} readOnly max={5} size='large' />
+                                        <Rating value={productInfo.rating} precision={0.5} readOnly max={5} size='large' />
                                     </Box>
                                     <Divider orientation="vertical" flexItem/>
                                     <Box width='30%' display='flex' justifyContent='center' alignItems='center' gap={2}>
                                         <IconButton color='primary' size="medium">
                                             <ShareIcon sx={{fontSize: '2rem'}}/>
                                         </IconButton>
-                                        <IconButton color='primary' size="medium">
-                                            <FavoriteIcon sx={{fontSize: '2rem'}}/>
+                                        <IconButton size="medium" onClick={addProductInLiked}>
+                                            <FavoriteIcon sx={{ fontSize: '2rem' }} color={isFavorite ? 'primary' : 'inherit'} />
                                         </IconButton>
                                     </Box>
                                 </Box>
                                 <Box flex={1} width='100%' display='flex'>
-                                    <Box width='10%'>
-
+                                    <Box width='10%' border='1px solid black'>
+                                            
                                     </Box> 
                                     <Box flex={1} display='flex' justifyContent='center' alignItems='center'>
-                                        <MyImage alt={productInfo?.title || ''} src={productInfo?.img || ''} width='290px' />
+                                        <MyImage alt={productInfo.title} src={productInfo.img} width='290px' />
                                     </Box>
                                 </Box>
                             </Box>
-                            <Box width='100%' height='25%' display='flex' justifyContent='center' alignItems='start' flexDirection='column' gap={1}>
+                            <Box width='100%' height='150px' display='flex' justifyContent='center' alignItems='start' flexDirection='column' gap={1}>
                                 <Typography variant="subtitle1" color='black' fontWeight='bold'>
                                     Consultar frete e prazo de entrega
                                 </Typography>
@@ -88,12 +128,23 @@ export function ProductPage() {
                         <Box flex={1} display='flex' flexDirection='column'>
                             <Box flex={1} width='100%' display='flex' flexDirection='column'>
                                 <Box flex={1}>
-                                    <Box height='60%' display='flex'>
-                                        <Box flex={1}> 
-
+                                    <Box height='60%' display='flex' flexDirection='column'>
+                                        <Box flex={1} display='flex' alignItems='center'> 
+                                            <Typography color='primary' variant="h2" fontWeight='bold'>
+                                                {productInfo.price}
+                                            </Typography>
                                         </Box>
-                                        <Box width='40%'>
-                                            
+                                        <Box width='50%' height='30%' display='flex' justifyContent='center'>
+                                            <Button 
+                                                variant="contained" 
+                                                fullWidth 
+                                                size="large" 
+                                                startIcon={isAlreadyInCart ? <ShoppingCartCheckoutIcon /> : <AddShoppingCartIcon />} 
+                                                sx={{fontSize:'1.5rem'}} 
+                                                onClick={() => addProductInCart(productInfo.id)}
+                                            >
+                                                {isAlreadyInCart ? 'NO CARRINHO' : 'COMPRAR'}
+                                            </Button>
                                         </Box>
                                     </Box>
                                 </Box>
@@ -106,8 +157,8 @@ export function ProductPage() {
                                     </Box>
                                 </Box>
                             </Box>
-                            <Box width='100%' height='25%'>
-                                {/* <Carousel showMiniCard /> */} 
+                            <Box width='620px' height='150px'>
+                                <Carousel showMiniCard /> 
                             </Box>
                         </Box>
                     </Box>
