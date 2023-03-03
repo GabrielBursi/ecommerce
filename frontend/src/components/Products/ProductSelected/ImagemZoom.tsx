fac1589687da732d497563a1c61a6b474e00f848
Gabriel Bursi <gabriellbursii@gmail.com>
2023-03-03 10:44:05 -0300

    fazendo o zoom na imagem de produto

--- frontend/src/components/Products/ProductSelected/ImagemZoom.tsx
+++ frontend/src/components/Products/ProductSelected/ImagemZoom.tsx
@@ -1,11 +1,72 @@
+import { useEffect, useState, MouseEvent } from "react";
 import { Box } from "@mui/material";
 import { MyImageProps } from "../../../types";
-import { MyImage } from "../MyImage";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export function ImagemZoom({alt, src}: MyImageProps) {
+
+    const [magnifyStyle, setMagnifyStyle] = useState({ backgroundImage: `url()` });
+
+    useEffect(() => {
+        setMagnifyStyle({ backgroundImage: `url(${src})` })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [src]);
+    
+
+    function handleMouseMove(e: MouseEvent<HTMLImageElement>){
+        
+        const { offsetX, offsetY } = e.nativeEvent
+        
+        const { clientX, clientY } = e
+
+        const xPercentage = (offsetX / clientX) * 100
+        const yPercentage = (offsetY / clientY) * 100
+
+        setMagnifyStyle((prev) => {
+            return {
+                    ...prev, 
+                    display: 'block',
+                    backgroundPosition: `${xPercentage}% ${yPercentage}%`,
+                    top: `${offsetY - 100}px`,
+                    left: `${offsetX - 100}px`
+                }
+        })
+        
+    }
+
+    function handleMouseLeave(_: React.MouseEvent<HTMLImageElement>){
+        setMagnifyStyle((prev) => ({...prev, display: 'none'}))
+    }
+
     return (
         <Box flex={1} display='flex' justifyContent='center' alignItems='center'>
-            <MyImage alt={alt} src={src} width='210px' />
+            <Box sx={{ position: 'relative', cursor: 'none' }}>
+                <LazyLoadImage
+                    alt={alt} 
+                    src={src} 
+                    width='250px' 
+                    height='auto' 
+                    onMouseMove={handleMouseMove}
+                    onMouseLeave={handleMouseLeave}
+                    draggable={false}
+                />
+                <Box
+                    sx={{
+                        display: 'none',
+                        position: 'absolute',
+                        backgroundRepeat: 'no-repeat',
+                        border: '0.6px solid black',
+                        width: '100px',
+                        height: '100px',
+                        backgroundSize: '300%',
+                        backgroundPosition: 'center',
+                        pointerEvents: 'none',
+                        borderRadius: '50%',
+                        ...magnifyStyle
+                    }}
+                >
+                </Box>
+            </Box>
         </Box>
     );
 }
