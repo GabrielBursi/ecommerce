000554b58e6616bc600319ea0502f8464a103e36
Gabriel Bursi <gabriellbursii@gmail.com>
2023-03-06 20:01:58 -0300

    construindo modal de cep

---
+++ frontend/src/components/Modal/ModalCEP.tsx
@@ -0,0 +1,114 @@
+import { Box, Button, IconButton, Rating, TextField, Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Modal from 'react-modal';
+import InputMask from "react-input-mask";
+import { useState } from 'react';
+
+interface ModalProps {
+    isOpen: boolean,
+    setIsOpen: (value: boolean) => void,
+    cep: string,
+    setCep: (cep: string) => void,
+    cepOptions: CepOptions[],
+}
+
+type CepOptions = {
+    name: string,
+    rating: number,
+    price: string,
+    days: number
+}
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+Modal.setAppElement('#root')
+
+export function ModalCEP({ isOpen, setIsOpen, cep, setCep, cepOptions }: ModalProps) {
+
+    const [showOptions, setShowOptions] = useState(true);
+
+    function showCepOptions() {
+        if (cep.length >= 9) {
+            setShowOptions(true)
+        } else {
+            setShowOptions(false)
+        }
+
+    }
+
+    return (
+        <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} style={customStyles}>
+            <Box display='flex' flexDirection='column' gap={1}>
+                <Box display='flex' justifyContent='space-between' alignItems='center' gap={2} height='20%'>
+                    <Box display='flex' alignItems='center' gap={1}>
+                        <LocalShippingIcon color='primary'/>
+                        <Typography variant='subtitle1' color='black' fontWeight='bold'>
+                            FRETE E PRAZO
+                        </Typography>
+                    </Box>
+                    <IconButton onClick={() => setIsOpen(false)}>
+                        <CloseIcon sx={{ fontSize: '0.8rem' }} />
+                    </IconButton>
+                </Box>
+                <Box display='flex' alignItems='center' gap={1} height='10%'>
+                    <LocationOnIcon color='primary' sx={{fontSize:'1.2rem'}}/>
+                    <Typography variant='body2' color='black'>
+                        CEP {cep}
+                    </Typography>
+                </Box>
+                <Box display='flex' justifyContent='space-between' alignItems='center' gap={2}>
+                    <Box flex={1} alignItems='center'>
+                        <InputMask
+                            mask="99999-999"
+                            value={cep}
+                            maskPlaceholder="_____-___"
+                            onChange={(e) => setCep(e.target.value)}
+                        >
+                            <TextField value={cep} fullWidth placeholder='Insira o CEP' />
+                        </InputMask>
+                    </Box>
+                    <Box display='flex' justifyContent='center' height='56px'>
+                        <Button variant='contained' sx={{height: '100%'}} onClick={showCepOptions}>ALTERAR CEP</Button>
+                    </Box>
+                </Box>
+                <Box display='flex' flexDirection='column' gap={1}>
+                    {showOptions && 
+                        cepOptions.map(option => {
+                            return (
+                                <Box key={option.name} display='flex' justifyContent='space-between' alignItems='center' mb={2}>
+                                    <Box display='flex' flexDirection='column' height='100%' alignItems='start' justifyContent='center'>
+                                        <Typography variant='subtitle1' color='black' fontWeight='bold'>
+                                            {option.name}
+                                        </Typography>
+                                        <Rating value={option.rating} precision={0.5} size='small' readOnly max={5} />
+                                    </Box>
+                                    <Box display='flex' flexDirection='column' height='100%' alignItems='start' justifyContent='center'>
+                                        <Typography variant='subtitle1' color='black' fontWeight='bold'>
+                                            {option.price}
+                                        </Typography>
+                                        <Typography variant='subtitle2' color='black' fontWeight='light'>
+                                            até {option.days} dias úteis
+                                        </Typography>
+                                    </Box>
+
+                                </Box>
+                            )
+                        })
+                    }
+                    
+                </Box>
+            </Box>
+        </Modal>
+    );
+}
--- frontend/src/components/Products/ProductSelected/SearchCep.tsx
+++ frontend/src/components/Products/ProductSelected/SearchCep.tsx
@@ -1,9 +1,29 @@
 import { Box, Button, TextField, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { useState } from "react";
+import InputMask from "react-input-mask";
+import { ModalCEP } from "../../Modal/ModalCEP";
+
+type CepOptions = {
+    name: string,
+    rating: number,
+    price: string,
+    days: number
+}
 
 export function SearchCep() {
 
     const theme = useTheme()
     const mdDown = useMediaQuery(theme.breakpoints.down('md'))
+
+    const [cep, setCep] = useState<string>('');
+    const [isOpen, setIsOpen] = useState(false);
+
+    const cepOptions: CepOptions[] = [
+        { name: 'Rede Sul', rating: 5, price: 'R$ 22,69', days: 4 }, 
+        { name: 'Sedex', rating: 4.5, price: 'R$ 23,12', days: 6 }, 
+        { name: 'GFL', rating: 5, price: 'R$ 30,24', days: 9 }, 
+        { name: 'Correios PAC', rating: 4.5, price: 'R$ 47,49', days: 5 },
+    ]
 
     return (
         <Box width='100%' height='150px' display='flex' justifyContent='center' alignItems='start' flexDirection='column' gap={1} >
@@ -12,14 +32,22 @@ export function SearchCep() {
             </Typography>
             <Box display='flex' gap={1} height='40%'>
                 <Box display='flex' justifyContent='center' alignItems='center' flex={1}>
-                    <TextField placeholder="Inserir CEP" />
+                    <InputMask
+                        mask="99999-999"
+                        value={cep}
+                        maskPlaceholder="_____-___"
+                        onChange={(e) => setCep(e.target.value)}
+                    >
+                        <TextField placeholder='Insira o CEP' />
+                    </InputMask>
                 </Box>
                 <Box display='flex' justifyContent='center' width='20%'>
-                    <Button variant="outlined" fullWidth sx={{ fontSize: mdDown ? '1rem' : '1.2rem' }}>
+                    <Button variant="outlined" fullWidth sx={{ fontSize: mdDown ? '1rem' : '1.2rem' }} onClick={() => cep.length === 9 && setIsOpen(true)}>
                         OK
                     </Button>
                 </Box>
             </Box>
+            <ModalCEP isOpen={isOpen} setIsOpen={setIsOpen} cep={cep} setCep={setCep} cepOptions={cepOptions}/>
         </Box>
     );
 }
