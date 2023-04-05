1e68d452e10679a84cf3b4008a7ee6b8c7576e9f
Gabriel Bursi <gabriellbursii@gmail.com>
2023-04-05 19:57:25 -0300

    middleware de autenticação com jwt

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { AddressController, ProductsController, UsersController } from "../controllers";
+import { ensureAuthenticated } from "../shared/middleware";
 
 export const router = express.Router();
 
@@ -8,8 +9,8 @@ router.post('/create', UsersController.createUserValidation, UsersController.Cre
 router.post('/login', UsersController.LoginUser)
 
 //!ADDRESS
-router.post('/address/new', AddressController.CreateNewAddress)
-router.get('/address/select', AddressController.SelectAddress)
+router.post('/address/new', ensureAuthenticated, AddressController.CreateNewAddress)
+router.get('/address/select', ensureAuthenticated, AddressController.SelectAddress)
 
 //!PRODUCTS
 router.get('/products', ProductsController.GetAllProducts)
