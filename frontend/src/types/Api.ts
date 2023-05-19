aac98e2fd574f34952a1d08034df1044211798b0
Gabriel Bursi <gabriellbursii@gmail.com>
2023-05-19 11:42:48 -0300

    arrumando tipagem

--- frontend/src/types/Api.ts
+++ frontend/src/types/Api.ts
@@ -1,7 +1,7 @@
 import { IAddress } from "./Address"
 
 export interface IProducts {
-    uuid: string, 
+    uuid: string,
     name: string,
     description?: string,
     price: number | string,
@@ -47,13 +47,12 @@ export interface IErrorAPI {
     response: {
         data: {
             errors: {
-                    default: string;
-                
+                default: string;
             }
         }
-        }
     }
+}
 
 export type LimitProductsPerPage = 20 | 40 | 60 | 80 | 100
 export type LimitProductsPerPageString = '20 por página' | '40 por página' | '60 por página' | '80 por página' | '100 por página'
-export type Category = 'pc gamer' | 'monitor gamer' | 'cadeira gamer' | 'mouse e teclado' | 'celular' | 'vídeo games' | 'geek' | 'TV' | 'tablets' | 'hardware' | 'periféricos' | 'home' | 'áudio' | 'serviços digitais e softwares' | 'câmeras e drones' | 'casa inteligente' | 'espaço gamer'
+export type Category = 'pc gamer' | 'monitor gamer' | 'cadeira gamer' | 'mouse e teclado' | 'celular' | 'vídeo games' | 'geek' | 'TV' | 'tablets' | 'hardware' | 'periféricos' | 'home' | 'áudio' | 'serviços digitais e softwares' | 'câmeras e drones' | 'casa inteligente' | 'espaço gamer' | undefined
