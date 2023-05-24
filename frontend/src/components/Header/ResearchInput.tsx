1538f3bfa0b33a78f78886f88015c562fb201e98
Gabriel Bursi <gabriellbursii@gmail.com>
2023-05-24 11:35:49 -0300

    input de pesquisa de produto

--- frontend/src/components/Header/ResearchInput.tsx
+++ frontend/src/components/Header/ResearchInput.tsx
@@ -1,44 +1,36 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { InputAdornment, TextField } from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
-import { useDebounce } from "../../hooks";
 
 export function ResearchInput(){
 
     const [productResearched, setProductResearched] = useState('');
     const navigate = useNavigate()
-    const { debounce } = useDebounce()
 
-    function handleChange(e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>){
-        const searchTerm = e.target.value;
-        setProductResearched(searchTerm);
-
-        if (searchTerm === '') {
-            navigate('/', { replace: true });
-        }else{
-            debounce(() => {
-                navigate(`/products/${searchTerm}`, {replace: true})
-            })
-        }
-
+    function researchProduct(e: FormEvent){
+        e.preventDefault()
+        setProductResearched('')
+        navigate(`/products/${productResearched}`)
     } 
 
     return (
-        <TextField
-            id="standard-search"
-            label="Pesquise aqui"
-            variant="standard"
-            fullWidth
-            value={productResearched}
-            onChange={handleChange}
-            InputProps={{
-                endAdornment: (
-                    <InputAdornment position="start">
-                        <SearchIcon color="primary" />
-                    </InputAdornment>
-                ),
-            }}
-        />
+        <form onSubmit={researchProduct} style={{ width: '100%' }}>
+            <TextField
+                id="standard-search"
+                label="Pesquise aqui"
+                variant="standard"
+                fullWidth
+                value={productResearched}
+                onChange={(e) => setProductResearched(e.target.value)}
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon color="primary" sx={{ cursor: 'pointer' }} onClick={researchProduct} />
+                        </InputAdornment>
+                    ),
+                }}
+            />
+        </form>
     );
 }
